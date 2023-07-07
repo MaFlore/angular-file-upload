a67c06f3a233a140188beb17b93b84d5d731fa19
MaFlore <yawo.ahadjitse@ipnetinstitute.com>
2023-07-07 07:52:05 +0000

    Création du component -articles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContenuComponent } from './templates/contenu/contenu.component';
 import { NavigationComponent } from './templates/navigation/navigation.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ArticlesComponent } from './articles/articles.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContenuComponent,
-    NavigationComponent
+    NavigationComponent,
+    ArticlesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
