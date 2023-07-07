36c967948005f9c6e3a4ab3fc41d248218c804c1
MaFlore <yawo.ahadjitse@ipnetinstitute.com>
2023-07-07 08:21:22 +0000

    Suppression du component -contenu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ArticlesComponent } from './articles/articles.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'', component:ArticlesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ContenuComponent } from './templates/contenu/contenu.component';
-import { NavigationComponent } from './templates/navigation/navigation.component';
+import { NavigationComponent } from './navigation/navigation.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ArticlesComponent } from './articles/articles.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContenuComponent,
     NavigationComponent,
     ArticlesComponent
   ],
