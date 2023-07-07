9bbc4b1008125f84e19f1b773c351d15a1d6f0e6
MaFlore <yawo.ahadjitse@ipnetinstitute.com>
2023-07-07 07:45:01 +0000

    Création du component -contenu et navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContenuComponent } from './templates/contenu/contenu.component';
+import { NavigationComponent } from './templates/navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContenuComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
